1592fcacdc79eadc5e5dfe6203d7da86eb568994
Jake <jacobstefan92@gmail.com>
2019-06-02 17:47:56 -0400

    added individual lead pages

--- controllers/landing.js
+++ controllers/landing.js
@@ -5,7 +5,7 @@ exports.get_landing = function(req, res, next) {
 };
 
 exports.submit_lead = function(req, res, next) {
-  console.log("lead_email", req.body.lead_email);
+  // console.log("lead_email", req.body.lead_email);
   return models.Lead.create({
     email: req.body.lead_email
   }).then(lead => {
@@ -16,5 +16,15 @@ exports.submit_lead = function(req, res, next) {
 exports.show_leads = function(req, res, next) {
   models.Lead.findAll().then(leads => {
     res.render("landing", { title: "Express", leads: leads });
+  });
+};
+
+exports.show_lead = function(req, res, next) {
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(lead => {
+    res.render("lead", { lead: lead });
   });
 };
